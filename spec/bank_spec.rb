9b5e24d9418fe20e1aac3049b12df02bf438b21a
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-07 12:25:19 +0000

    Wrote test for statement table

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -36,4 +36,16 @@ describe Bank do
       expect { subject.withdraw(15) }.to raise_error('Insufficient balance!')
     end
   end
+
+  describe '#statement' do
+    it 'produces a horrible looking table' do
+      subject.deposit(1000)
+      subject.deposit(2000)
+      subject.withdraw(500)
+      expect(subject.statement).to eq('date || credit || debit || balance
+14/01/2012 || || 500.00 || 2500.00
+13/01/2012 || 2000.00 || || 3000.00
+10/01/2012 || 1000.00 || || 1000.00')
+    end
+  end
 end
