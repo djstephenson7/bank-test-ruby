a252b2e70c6d96a51940fb5adb899e3e2365592a
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-07 12:14:12 +0000

    Rewrote tests for output, not print

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,16 +5,6 @@ describe Bank do
     Bank.any_instance.stub(:puts)
   end
 
-  describe 'balance' do
-    it 'Has a starting balance of £0' do
-      expect(subject.balance).to eq 0
-    end
-
-    it 'Has a transaction history' do
-      expect(subject.transaction).to include(:debit, :credit, :balance, :date)
-    end
-  end
-
   describe '#deposit' do
     it 'Can credit money to the balance' do
       expect { subject.deposit(1000) }.to change(subject, :balance).by(+1000)
@@ -22,9 +12,8 @@ describe Bank do
 
     it 'Displays a deposit transaction hash' do
       subject.deposit(100)
-      expect do
-        print(debit: 0, credit: 100, balance: 100, date: Date.today.to_s)
-      end.to output.to_stdout
+      expect(subject.history)
+        .to eq([debit: 0, credit: 100, balance: 100, date: Date.today.to_s])
     end
   end
 
@@ -37,9 +26,9 @@ describe Bank do
     it 'Displays a withdraw transaction hash' do
       subject.deposit(100)
       subject.withdraw(50)
-      expect do
-        print(debit: 50, credit: 100, balance: 50, date: Date.today.to_s)
-      end.to output.to_stdout
+      expect(subject.history)
+        .to eq([{ balance: 100, credit: 100, debit: 0, date: Date.today.to_s },
+                { balance: 50, credit: 0, debit: 50, date: Date.today.to_s }])
     end
 
     it 'raises an error if the user goes below 0' do
