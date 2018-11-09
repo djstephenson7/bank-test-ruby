2f5ac04760e6c4786686b650c1e9882e260ca13a
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-09 14:22:30 +0000

    Overdraft limit is stored

--- lib/overdraft.rb
+++ lib/overdraft.rb
@@ -1,3 +1,13 @@
+# The Overdraft class
 class Overdraft
+  attr_reader :default_limit, :customer_limit
 
+  def initialize
+    @default_limit = 0
+    @customer_limit = 0
+  end
+
+  def new_limit(limit)
+    @customer_limit = limit
+  end
 end
--- spec/overdraft_spec.rb
+++ spec/overdraft_spec.rb
@@ -1,20 +1,14 @@
 require 'overdraft'
 describe Overdraft do
-  # Arrange
   describe 'overdraft facilities' do
     it 'has a starting value of 0' do
-      # Act
-      # Assert
       expect(subject.default_limit).to eq 0
     end
   end
 
-  # Arrange
   describe '#set_limit' do
     it 'allows the user to set their overdraft limit by passing an argument' do
-      # Act
-      # Assert
-      subject.set_limit(-50)
+      subject.new_limit(-50)
       expect(subject.customer_limit).to eq(-50)
     end
   end
